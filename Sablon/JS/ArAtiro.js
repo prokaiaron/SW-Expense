f2f7d701ae486b266f6ef1551b332a9757c7de92
prokaiaron <prokagaming@gmail.com>
2024-01-07 17:57:42 +0100

    Shop árak

--- Sablon/JS/ArAtiro.js
+++ Sablon/JS/ArAtiro.js
@@ -5,40 +5,84 @@ function ArAtiras() {
 
     let penznem = JSON.parse(localStorage.getItem("penznem"));
     
-    let nevek = document.querySelectorAll(".product-name");
-    let arak = document.querySelectorAll(".product-price");
+    let mainnevek = document.querySelectorAll(".product-name");
+    let mainarak = document.querySelectorAll(".product-price");
 
-    for (let i = 0; i < nevek.length; i++) {
-        let nev = nevek[i].textContent.charAt(0);
+    let shopnevek = document.querySelectorAll(".shop-nev");
+    let shoparak = document.querySelectorAll(".shop-ar")
+
+
+
+    for (let i = 0; i < mainnevek.length; i++) {
+        let nev = mainnevek[i].textContent.charAt(0);
 
         if (penznem == "huf") 
         {
             if (nev == "F") 
             {
-                arak[i].innerHTML = flowerar + " Ft";
+                mainarak[i].innerHTML = flowerar + " Ft";
             }
             if (nev == "C") 
             {
-                arak[i].innerHTML = corear + " Ft";
+                mainarak[i].innerHTML = corear + " Ft";
             }
             if (nev == "S") 
             {
-                arak[i].innerHTML = streetar + " Ft";
+                mainarak[i].innerHTML = streetar + " Ft";
             }
         }
         if (penznem == "eur") 
         {
             if (nev == "F") 
             {
-                arak[i].innerHTML = Math.round(flowerar / 380) + "€";
+                mainarak[i].innerHTML = Math.round(flowerar / 380) + "€";
             }
             if (nev == "C") 
             {
-                arak[i].innerHTML = Math.round(corear / 380) + "€";
+                mainarak[i].innerHTML = Math.round(corear / 380) + "€";
             }
             if (nev == "S") 
             {
-                arak[i].innerHTML = Math.round(streetar / 380) + "€";
+                mainarak[i].innerHTML = Math.round(streetar / 380) + "€";
+            }
+        }
+    }
+
+
+
+
+
+    for (let i = 0; i < shopnevek.length; i++) {
+        let nev = shopnevek[i].textContent.charAt(0);
+
+        if (penznem == "huf") 
+        {
+            if (nev == "F") 
+            {
+                shoparak[i].innerHTML = flowerar + " Ft";
+            }
+            if (nev == "C") 
+            {
+                shoparak[i].innerHTML = corear + " Ft";
+            }
+            if (nev == "S") 
+            {
+                shoparak[i].innerHTML = streetar + " Ft";
+            }
+        }
+        if (penznem == "eur") 
+        {
+            if (nev == "F") 
+            {
+                shoparak[i].innerHTML = Math.round(flowerar / 380) + "€";
+            }
+            if (nev == "C") 
+            {
+                shoparak[i].innerHTML = Math.round(corear / 380) + "€";
+            }
+            if (nev == "S") 
+            {
+                shoparak[i].innerHTML = Math.round(streetar / 380) + "€";
             }
         }
     }
